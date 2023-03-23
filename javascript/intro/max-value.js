90b7f7ee4371226c20cda77ab1449a453d209484
skyline502 <johnny.san@gmail.com>
2023-03-22 22:19:18 -0700

    solved max value

--- javascript/intro/max-value.js
+++ javascript/intro/max-value.js
@@ -7,22 +7,45 @@ Solve this without using any built-in array methods.
 
 You can assume that the array is non-empty.
 
-test_00:
-maxValue([4, 7, 2, 8, 10, 9]); // -> 10
-test_01:
-maxValue([10, 5, 40, 40.3]); // -> 40.3
-test_02:
-maxValue([-5, -2, -1, -11]); // -> -1
-test_03:
-maxValue([42]); // -> 42
-test_04:
-maxValue([1000, 8]); // -> 1000
-test_05:
-maxValue([1000, 8, 9000]); // -> 9000
-test_06:
-maxValue([2, 5, 1, 1, 4]); // -> 5
+// test_00:
+console.log(maxValue([4, 7, 2, 8, 10, 9]); // -> 10
+// test_01:
+console.log(maxValue([10, 5, 40, 40.3]); // -> 40.3
+// test_02:
+console.log(maxValue([-5, -2, -1, -11]); // -> -1
+// test_03:
+console.log(maxValue([42]); // -> 42
+// test_04:
+console.log(maxValue([1000, 8]); // -> 1000
+// test_05:
+console.log(maxValue([1000, 8, 9000]); // -> 9000
+// test_06:
+console.log(maxValue([2, 5, 1, 1, 4]); // -> 5
 */
 
 const maxValue = arr => {
+  let max = -Infinity;
 
+  for (let val of arr) {
+    if (val > max) {
+      max = val;
+    }
+  }
+
+  return max;
 }
+
+// test_00:
+console.log(maxValue([4, 7, 2, 8, 10, 9])); // -> 10
+// test_01:
+console.log(maxValue([10, 5, 40, 40.3])); // -> 40.3
+// test_02:
+console.log(maxValue([-5, -2, -1, -11])); // -> -1
+// test_03:
+console.log(maxValue([42])); // -> 42
+// test_04:
+console.log(maxValue([1000, 8])); // -> 1000
+// test_05:
+console.log(maxValue([1000, 8, 9000])); // -> 9000
+// test_06:
+console.log(maxValue([2, 5, 1, 1, 4])); // -> 5
