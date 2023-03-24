2a41bd03aaf5604d48ea4e8723481c79ca8d1b43
Johnny San <johnny.san@gmail.com>
2023-03-24 10:01:22 -0700

    added most frequent character

---
+++ javascript/array-and-string/mostFrequentChar.js
@@ -0,0 +1,29 @@
+/*
+most frequent char
+Write a function, mostFrequentChar, that takes in a string as
+an argument. The function should return the most frequent character
+of the string. If there are ties, return the character that appears
+earlier in the string.
+
+You can assume that the input string is non-empty.
+*/
+
+const mostFrequentChar = str => {
+
+}
+
+
+// test_00:
+console.log(mostFrequentChar('bookeeper')); // -> 'e'
+// test_01:
+console.log(mostFrequentChar('david')); // -> 'd'
+// test_02:
+console.log(mostFrequentChar('abby')); // -> 'b'
+// test_03:
+console.log(mostFrequentChar('mississippi')); // -> 'i'
+// test_04:
+console.log(mostFrequentChar('potato')); // -> 'o'
+// test_05:
+console.log(mostFrequentChar('eleventennine')); // -> 'e'
+// test_06:
+console.log(mostFrequentChar("riverbed")); // -> 'r'
