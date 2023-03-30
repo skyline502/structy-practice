65326aa844afb3a5e0ca3be70236cde852794660
Johnny San <johnny.san@gmail.com>
2023-03-30 09:50:49 -0700

    added linked list find

---
+++ javascript/linked-lists/linked-list-find.js
@@ -0,0 +1,73 @@
+/*
+linked list find
+
+Write a function, linkedListFind, that takes in the head of a
+linked list and a target value. The function should return a
+boolean indicating whether or not the linked list contains the target.
+
+
+*/
+
+const linkedListFind = head => {
+
+}
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+// test_00:
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+// a -> b -> c -> d
+
+console.log(linkedListFind(a, "c")); // true
+// test_01:
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+// a -> b -> c -> d
+
+console.log(linkedListFind(a, "d")); // true
+// test_02:
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+// a -> b -> c -> d
+
+console.log(linkedListFind(a, "q")); // false
+// test_03:
+const node1 = new Node("jason");
+const node2 = new Node("leneli");
+
+node1.next = node2;
+
+// jason -> leneli
+
+console.log(linkedListFind(node1, "jason")); // true
+// test_04:
+const node1 = new Node(42);
+
+// 42
+
+console.log(linkedListFind(node1, 42)); // true
+// test_05:
+const node1 = new Node(42);
+
+// 42
+
+console.log(linkedListFind(node1, 100)); // false
