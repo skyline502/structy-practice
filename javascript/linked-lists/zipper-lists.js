c2e5f8f6db1ab28d8f10c6464040ea776d3fc9f3
Johnny San <johnny.san@gmail.com>
2023-04-03 09:57:38 -0700

    solved zipper lists

--- javascript/linked-lists/zipper-lists.js
+++ javascript/linked-lists/zipper-lists.js
@@ -1,7 +1,12 @@
 /*
 
 zipper lists
-Write a function, zipperLists, that takes in the head of two linked lists as arguments. The function should zipper the two lists together into single linked list by alternating nodes. If one of the linked lists is longer than the other, the resulting list should terminate with the remaining nodes. The function should return the head of the zippered linked list.
+Write a function, zipperLists, that takes in the head of
+two linked lists as arguments. The function should zipper the
+two lists together into single linked list by alternating nodes.
+If one of the linked lists is longer than the other, the resulting
+list should terminate with the remaining nodes. The function should
+return the head of the zippered linked list.
 
 Do this in-place, by mutating the original Nodes.
 
@@ -17,7 +22,20 @@ class Node {
 }
 
 const zipperLists = (h1, h2) => {
+  let current = h1;
+  let next = h2;
 
+  while (current) {
+    let tempNext = current.next;
+    if (next) {
+      current.next = next;
+    } else {
+      break;
+    }
+    current = next;
+    next = tempNext;
+  }
+  return head1;
 }
 
 // test_00:
