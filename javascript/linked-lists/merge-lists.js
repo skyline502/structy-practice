09f0633c0df4f34785ca52263b2188502547cc30
Johnny San <johnny.san@gmail.com>
2023-04-06 10:33:22 -0700

    solved merge lists

--- javascript/linked-lists/merge-lists.js
+++ javascript/linked-lists/merge-lists.js
@@ -18,36 +18,38 @@ class Node {
   }
 }
 
-const mergeLists = (h1, h2) => {
-  let head;
-  let current;
-  let next;
+const mergeLists = (head1, head2) => {
+  let h1 = head1;
+  let h2= head2;
+  let start;
 
-  if (h1.val > h2.val) {
-    next = h1;
-    current = h2;
-    head = h2;
+  if (h1.val < h2.val) {
+    start = h1;
+    h1 = head1.next;
   } else {
-    next = h2;
-    current = h1;
-    head = h1
+    start = h2;
+    h2 = head2.next;
   }
 
-  while (current) {
-    let tempNext = current.next;
-    if (tempNext < next) {
-      current = tempNext;
+  let current = start;
+  while (h1 !== null && h2 !== null) {
+    if (h1.val < h2.val) {
+      current.next = h1;
+      h1 = h1.next;
     } else {
-      current = next;
-      next = tempNext;
+      current.next = h2;
+      h2 = h2.next;
     }
+    current = current.next;
   }
 
-  if (next) {
-    current.next
+  if (!h1) {
+    current.next = h2;
+  } else {
+    current.next = h1;
   }
 
-  let results = linkedListValues(head);
+  let results = linkedListValues(start);
   return results;
 }
 
