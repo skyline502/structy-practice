904cd7c1266bb589fa427ad366f61c3c100f3ab5
Johnny San <johnny.san@gmail.com>
2023-04-26 10:36:52 -0700

    solved fivesort in python

--- python/array-and-string/fivesort.py
+++ python/array-and-string/fivesort.py
@@ -8,6 +8,20 @@
 # Elements that are not 5 can appear in any order in the output,
 # as long as all 5s are at the end of the array.
 
+def fiveSort(a):
+  i = 0
+  j = len(a) - 1
+
+  while i < j:
+    if a[j] == 5:
+      j -= 1
+    elif a[i] == 5:
+      a[i], a[j] = a[j], a[i]
+    else:
+      i += 1
+
+  return a
+
 # Examples
 # test_00
 print(fiveSort([12, 5, 1, 5, 12, 7]));
@@ -25,8 +39,8 @@ print(fiveSort([5, 5, 6, 5, 5, 5, 5]));
 print(fiveSort([5, 1, 2, 5, 5, 3, 2, 5, 1, 5, 5, 5, 4, 5]));
 # [4, 1, 2, 1, 2, 3, 5, 5, 5, 5, 5, 5, 5, 5]
 # test_05
-fives = np.fill(5);
-fours = np.fill(4);
+fives = [5] * 2000;
+fours = [4] * 2000;
 nums = [*fives, *fours]
 print(fiveSort(nums))
 # twenty-thousand 4s followed by twenty-thousand 5s
