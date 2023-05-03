c55c4bb3271b3f10d11ac41b733a644c95bd9d74
skyline502 <johnny.san@gmail.com>
2023-05-03 10:28:50 -0700

    solved how high

--- javascript/binary-tree/how-high.js
+++ javascript/binary-tree/how-high.js
@@ -75,6 +75,70 @@ test_04:
 howHigh(null); // -> -1
 */
 
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
 const howHigh = root => {
   if (!root) return -1;
+  let left = howHigh(root?.left) || 0
+  let right = howHigh(root?.right) || 0
+
+  return 1 + Math.max(left, right);
+
 }
+
+//Examples:
+
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+
+console.log(howHigh(a)); // -> 2
+console.log(howHigh(null)); // -> -1
+
+const g = new Node('g');
+e.left = g
+
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+//    /
+//   g
+
+console.log(howHigh(a)); // -> 3
+
+const h = new Node('h');
+const i = new Node('i');
+
+h.right = i;
+
+//      a
+//       \
+//        c
+
+console.log(howHigh(h)); // -> 1
+
+const j = new Node('j');
+console.log(howHigh(j)) // -> 0
