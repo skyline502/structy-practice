2cf7b03d49dba69a568686c339369678ef72781b
skyline502 <johnny.san@gmail.com>
2023-05-04 12:57:36 -0700

    solved bottom right val

--- javascript/binary-tree/bottom-right-value.js
+++ javascript/binary-tree/bottom-right-value.js
@@ -1,7 +1,9 @@
 /*
 
 bottom right value
-Write a function, bottomRightValue, that takes in the root of a binary tree. The function should return the right-most value in the bottom-most level of the tree.
+
+Write a function, bottomRightValue, that takes in the root of a binary tree.
+The function should return the right-most value in the bottom-most level of the tree.
 
 You may assume that the input tree is non-empty.
 
@@ -125,27 +127,130 @@ class Node {
 }
 
 const bottomRightValue = root => {
+  let queue = [root];
+
+  while (queue.length) {
+    let current = queue.shift();
+
+    if (!current.left && !current.right && !queue.length) return current.val;
+
+    if (current.left) {
+      queue.push(current.left);
+    }
+
+    if (current.right) {
+      queue.push(current.right);
+    }
+  }
 
 }
 
 //Examples:
-const a = new Node(3);
-const b = new Node(11);
-const c = new Node(10);
-const d = new Node(4);
-const e = new Node(-2);
-const f = new Node(1);
+// const a = new Node(3);
+// const b = new Node(11);
+// const c = new Node(10);
+// const d = new Node(4);
+// const e = new Node(-2);
+// const f = new Node(1);
+
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+
+// //       3
+// //    /    \
+// //   11     10
+// //  / \      \
+// // 4   -2     1
+
+// console.log(bottomRightValue(a)); // -> 1
+
+// const a = new Node(-1);
+// const b = new Node(-6);
+// const c = new Node(-5);
+// const d = new Node(-3);
+// const e = new Node(-4);
+// const f = new Node(-13);
+// const g = new Node(-2);
+// const h = new Node(6);
+
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+// e.left = g;
+// e.right = h;
+
+// //        -1
+// //      /   \
+// //    -6    -5
+// //   /  \     \
+// // -3   -4   -13
+// //     / \
+// //    -2  6
+
+// bottomRightValue(a); // -> 6
+// test_02:
+// const a = new Node(-1);
+// const b = new Node(-6);
+// const c = new Node(-5);
+// const d = new Node(-3);
+// const e = new Node(-4);
+// const f = new Node(-13);
+// const g = new Node(-2);
+// const h = new Node(6);
+// const i = new Node(7);
+
+
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+// e.left = g;
+// e.right = h;
+// f.left = i;
+
+// //        -1
+// //      /   \
+// //    -6    -5
+// //   /  \     \
+// // -3   -4   -13
+// //     / \    /
+// //    -2  6  7
+
+// bottomRightValue(a); // -> 7
+// test_03
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
 
 a.left = b;
 a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
+b.right = d;
+d.left = e;
+e.right = f;
 
-//       3
-//    /    \
-//   11     10
-//  / \      \
-// 4   -2     1
+//      a
+//    /   \
+//   b     c
+//    \
+//     d
+//    /
+//   e
+//  /
+// f
 
-console.log(bottomRightValue(a)); // -> 1
+console.log(bottomRightValue(a)); // -> 'f'
+// test_04
+const j = new Node(42);
+
+//      42
+
+console.log(bottomRightValue(j)); // -> 42
