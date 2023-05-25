dcab5b1afe13d294b92b47dfddeed6239fb9054e
Johnny San <johnny.san@gmail.com>
2023-05-25 10:03:00 -0700

    solved create linked list.py

--- python/linked-list/create-linked-list.py
+++ python/linked-list/create-linked-list.py
@@ -1,16 +1,45 @@
 # create linked list
 
-# Write a function, createLinkedList, that takes in an array of values as an argument.
-# The function should create a linked list containing each element of the array as the
-# values of the nodes. The function should return the head of the linked list.
+# Write a function, createLinkedList, that takes in an array of values as an
+# argument.
+
+# The function should create a linked list containing each element of the array as
+# the values of the nodes. The function should return the head of the linked list.
 
 class Node:
   def __init__(self, val):
     self.val = val
     self.next = None
 
+
+def linkedListValues(head):
+   result = []
+
+   while head:
+      result.append(head.val)
+      head = head.next
+
+   return result
+
+
 def createLinkedList(list):
-  print(list)
+  if len(list) < 1:
+    return None
+
+  head = None
+  cur = None
+
+  for el in list:
+    if head == None:
+      head = Node(el)
+      cur = head
+    else:
+      n = Node(el)
+      cur.next = n
+      cur = n
+
+  return linkedListValues(head)
+
 
 # Examples:
 # test_00:
