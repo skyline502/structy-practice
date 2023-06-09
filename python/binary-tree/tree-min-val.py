6a98fa37c891904405996303782f88ce0872f61c
Johnny San <johnny.san@gmail.com>
2023-06-09 09:28:12 -0700

    solved tree-min-val.py

--- python/binary-tree/tree-min-val.py
+++ python/binary-tree/tree-min-val.py
@@ -15,17 +15,30 @@ class Node:
 
 def treeMinValue(root):
   min_val = float('inf')
+  stack = [root]
 
-  print(root)
+  while len(stack) > 0:
+    current = stack.pop()
+
+    if current.val < min_val:
+      min_val = current.val
+
+    if current.right:
+      stack.append(current.right)
+
+    if current.left:
+      stack.append(current.left)
+
+  return min_val
 
 
 # test_00:
-a = Node(3);
-b = Node(11);
-c = Node(4);
-d = Node(4);
-e = Node(-2);
-f = Node(1);
+a = Node(3)
+b = Node(11)
+c = Node(4)
+d = Node(4)
+e = Node(-2)
+f = Node(1)
 
 a.left = b;
 a.right = c;
@@ -39,20 +52,20 @@ c.right = f;
 #  / \      \
 # 4   -2     1
 
-print(treeMinValue(a)); # -> -2
+print(treeMinValue(a)) # -> -2
 # test_01:
-# a = Node(5);
-# b = Node(11);
-# c = Node(3);
-# d = Node(4);
-# e = Node(14);
-# f = Node(12);
+a = Node(5);
+b = Node(11);
+c = Node(3);
+d = Node(4);
+e = Node(14);
+f = Node(12);
 
-# a.left = b;
-# a.right = c;
-# b.left = d;
-# b.right = e;
-# c.right = f;
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
 
 # #       5
 # #    /    \
@@ -60,24 +73,24 @@ print(treeMinValue(a)); # -> -2
 # #  / \      \
 # # 4   14     12
 
-# print(treeMinValue(a)); # -> 3
+print(treeMinValue(a)); # -> 3
 # # test_02:
-# a = Node(-1);
-# b = Node(-6);
-# c = Node(-5);
-# d = Node(-3);
-# e = Node(-4);
-# f = Node(-13);
-# g = Node(-2);
-# h = Node(-2);
+a = Node(-1);
+b = Node(-6);
+c = Node(-5);
+d = Node(-3);
+e = Node(-4);
+f = Node(-13);
+g = Node(-2);
+h = Node(-2);
 
-# a.left = b;
-# a.right = c;
-# b.left = d;
-# b.right = e;
-# c.right = f;
-# e.left = g;
-# f.right = h;
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+e.left = g;
+f.right = h;
 
 # #        -1
 # #      /   \
@@ -87,7 +100,7 @@ print(treeMinValue(a)); # -> -2
 # #     /       \
 # #    -2       -2
 
-# print(treeMinValue(a)); # -> -13
+print(treeMinValue(a)); # -> -13
 # # test_03:
 # a = Node(42);
 
