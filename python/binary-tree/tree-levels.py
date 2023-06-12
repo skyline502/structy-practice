c718cb261db7d308b0757564a1b79747c7cad79c
Johnny San <johnny.san@gmail.com>
2023-06-12 09:39:11 -0700

    added tree-levels.py

---
+++ python/binary-tree/tree-levels.py
@@ -0,0 +1,113 @@
+# tree levels
+
+# Write a function, treeLevels, that takes in the root of a binary tree.
+# The function should return a 2-Dimensional array where each subarray represents
+# a level of the tree.
+
+class Node:
+  def __init__(self,val):
+    self.val = val
+    self.left = None
+    self.right = None
+
+def treeLevels(root):
+  levels = []
+  print(root.val)
+
+# Examples:
+
+# test_00:
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+treeLevels(a) # ->
+# [
+#   ['a'],
+#   ['b', 'c'],
+#   ['d', 'e', 'f']
+# ]
+# test_01:
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+g = Node("g")
+h = Node("h")
+i = Node("i")
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+e.left = g
+e.right = h
+f.left = i
+
+#         a
+#      /    \
+#     b      c
+#   /  \      \
+#  d    e      f
+#      / \    /
+#     g  h   i
+
+treeLevels(a) # ->
+# [
+#   ['a'],
+#   ['b', 'c'],
+#   ['d', 'e', 'f'],
+#   ['g', 'h', 'i']
+# ]
+# test_02:
+q = Node("q")
+r = Node("r")
+s = Node("s")
+t = Node("t")
+u = Node("u")
+v = Node("v")
+
+q.left = r
+q.right = s
+r.right = t
+t.left = u
+u.right = v
+
+#      q
+#    /   \
+#   r     s
+#    \
+#     t
+#    /
+#   u
+#  /
+# v
+
+treeLevels(q) #->
+# [
+#   ['q'],
+#   ['r', 's'],
+#   ['t'],
+#   ['u'],
+#   ['v']
+# ]
+# test_03:
+treeLevels(null) # -> []
