b41481d486416e0acc15b4724b85b764ab3113d3
Johnny San <johnny.san@gmail.com>
2023-06-22 10:03:21 -0700

    added best bridge.py

---
+++ python/graphs/best-bridge.py
@@ -0,0 +1,89 @@
+# best bridge
+
+# Write a function, print(bestBridge, that) takes in a grid as an argument. The grid contains water (W) and land (L). There are exactly two islands in the grid.
+# An island is a vertically or horizontally connected region of land. Return the minimum length bridge needed to connect the two islands.
+# A bridge does not need to form a straight line.
+
+def bestBridge(grid):
+  print(grid, 'da grid')
+
+# test_00:
+
+grid = [
+  ["W", "W", "W", "L", "L"],
+  ["L", "L", "W", "W", "L"],
+  ["L", "L", "L", "W", "L"],
+  ["W", "L", "W", "W", "W"],
+  ["W", "W", "W", "W", "W"],
+  ["W", "W", "W", "W", "W"],
+]
+print(bestBridge(grid)) # -> 1
+
+# test_01:
+
+grid = [
+  ["W", "W", "W", "W", "W"],
+  ["W", "W", "W", "W", "W"],
+  ["L", "L", "W", "W", "L"],
+  ["W", "L", "W", "W", "L"],
+  ["W", "W", "W", "L", "L"],
+  ["W", "W", "W", "W", "W"],
+]
+print(bestBridge(grid)) # -> 2
+
+# test_02:
+
+grid = [
+  ["W", "W", "W", "W", "W"],
+  ["W", "W", "W", "L", "W"],
+  ["L", "W", "W", "W", "W"],
+]
+print(bestBridge(grid)) # -> 3
+
+# test_03:
+
+grid = [
+  ["W", "W", "W", "W", "W", "W", "W", "W"],
+  ["W", "W", "W", "W", "W", "W", "W", "W"],
+  ["W", "W", "W", "W", "W", "W", "W", "W"],
+  ["W", "W", "W", "W", "W", "L", "W", "W"],
+  ["W", "W", "W", "W", "L", "L", "W", "W"],
+  ["W", "W", "W", "W", "L", "L", "L", "W"],
+  ["W", "W", "W", "W", "W", "L", "L", "L"],
+  ["L", "W", "W", "W", "W", "L", "L", "L"],
+  ["L", "L", "L", "W", "W", "W", "W", "W"],
+  ["W", "W", "W", "W", "W", "W", "W", "W"],
+]
+print(bestBridge(grid)) # -> 3
+
+# test_04:
+
+grid = [
+  ["L", "L", "L", "L", "L", "L", "L", "L"],
+  ["L", "W", "W", "W", "W", "W", "W", "L"],
+  ["L", "W", "W", "W", "W", "W", "W", "L"],
+  ["L", "W", "W", "W", "W", "W", "W", "L"],
+  ["L", "W", "W", "W", "W", "W", "W", "L"],
+  ["L", "W", "W", "W", "W", "W", "W", "L"],
+  ["L", "W", "W", "L", "W", "W", "W", "L"],
+  ["L", "W", "W", "W", "W", "W", "W", "L"],
+  ["L", "W", "W", "W", "W", "W", "W", "L"],
+  ["L", "W", "W", "W", "W", "W", "W", "L"],
+  ["L", "W", "W", "W", "W", "W", "W", "L"],
+  ["L", "L", "L", "L", "L", "L", "L", "L"],
+]
+print(bestBridge(grid)) # -> 2
+
+# test_05:
+
+grid = [
+  ["W", "L", "W", "W", "W", "W", "W", "W"],
+  ["W", "L", "W", "W", "W", "W", "W", "W"],
+  ["W", "W", "W", "W", "W", "W", "W", "W"],
+  ["W", "W", "W", "W", "W", "W", "W", "W"],
+  ["W", "W", "W", "W", "W", "W", "W", "W"],
+  ["W", "W", "W", "W", "W", "W", "L", "W"],
+  ["W", "W", "W", "W", "W", "W", "L", "L"],
+  ["W", "W", "W", "W", "W", "W", "W", "L"],
+]
+print(bestBridge(grid)) # -> 8
