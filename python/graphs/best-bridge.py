b84a2dba3a27bc02d006cc1c662dfa33f4ee6738
Johnny San <johnny.san@gmail.com>
2023-06-27 10:17:00 -0700

    getting recursion error

--- python/graphs/best-bridge.py
+++ python/graphs/best-bridge.py
@@ -11,16 +11,44 @@ def bestBridge(grid):
    for col, column in enumerate(rows):
     possible_island = find_island(grid, row, col, set())
 
-    if possible_island.size > 0:
+    if len(possible_island) > 0:
       print(possible_island, 'island found')
       island = possible_island
       break
 
-visited = set()
+  visited = set()
+  queue = []
+
+  for pos in island:
+    print(pos, 'current pos')
+
+
+def is_inbounds(grid, row, col):
+  row_inbounds = row >= 0 and row < len(grid)
+  col_inbounds = 0 <= col and col < len(grid[0])
+
+  print(row_inbounds, 'rows', row, col)
+  print(col_inbounds, 'col', row, col)
+
+  return row_inbounds and col_inbounds
+
 
 
 def find_island(grid, row, col, visited):
-  print(grid, row, col, visited)
+  if is_inbounds(grid, row, col) == False or grid[row][col] == 'W':
+    return visited
+
+  node = str(row) + ',' + str(col)
+
+  visited.add(node)
+
+  find_island(grid, row + 1, col, visited)
+  find_island(grid, row - 1, col, visited)
+  find_island(grid, row, col + 1, visited)
+  find_island(grid, row, col - 1, visited)
+
+  return visited
+
 # test_00:
 
 grid = [
@@ -35,69 +63,69 @@ print(bestBridge(grid)) # -> 1
 
 # test_01:
 
-grid = [
-  ["W", "W", "W", "W", "W"],
-  ["W", "W", "W", "W", "W"],
-  ["L", "L", "W", "W", "L"],
-  ["W", "L", "W", "W", "L"],
-  ["W", "W", "W", "L", "L"],
-  ["W", "W", "W", "W", "W"],
-]
-print(bestBridge(grid)) # -> 2
+# grid = [
+#   ["W", "W", "W", "W", "W"],
+#   ["W", "W", "W", "W", "W"],
+#   ["L", "L", "W", "W", "L"],
+#   ["W", "L", "W", "W", "L"],
+#   ["W", "W", "W", "L", "L"],
+#   ["W", "W", "W", "W", "W"],
+# ]
+# print(bestBridge(grid)) # -> 2
 
-# test_02:
+# # test_02:
 
-grid = [
-  ["W", "W", "W", "W", "W"],
-  ["W", "W", "W", "L", "W"],
-  ["L", "W", "W", "W", "W"],
-]
-print(bestBridge(grid)) # -> 3
+# grid = [
+#   ["W", "W", "W", "W", "W"],
+#   ["W", "W", "W", "L", "W"],
+#   ["L", "W", "W", "W", "W"],
+# ]
+# print(bestBridge(grid)) # -> 3
 
-# test_03:
+# # test_03:
 
-grid = [
-  ["W", "W", "W", "W", "W", "W", "W", "W"],
-  ["W", "W", "W", "W", "W", "W", "W", "W"],
-  ["W", "W", "W", "W", "W", "W", "W", "W"],
-  ["W", "W", "W", "W", "W", "L", "W", "W"],
-  ["W", "W", "W", "W", "L", "L", "W", "W"],
-  ["W", "W", "W", "W", "L", "L", "L", "W"],
-  ["W", "W", "W", "W", "W", "L", "L", "L"],
-  ["L", "W", "W", "W", "W", "L", "L", "L"],
-  ["L", "L", "L", "W", "W", "W", "W", "W"],
-  ["W", "W", "W", "W", "W", "W", "W", "W"],
-]
-print(bestBridge(grid)) # -> 3
+# grid = [
+#   ["W", "W", "W", "W", "W", "W", "W", "W"],
+#   ["W", "W", "W", "W", "W", "W", "W", "W"],
+#   ["W", "W", "W", "W", "W", "W", "W", "W"],
+#   ["W", "W", "W", "W", "W", "L", "W", "W"],
+#   ["W", "W", "W", "W", "L", "L", "W", "W"],
+#   ["W", "W", "W", "W", "L", "L", "L", "W"],
+#   ["W", "W", "W", "W", "W", "L", "L", "L"],
+#   ["L", "W", "W", "W", "W", "L", "L", "L"],
+#   ["L", "L", "L", "W", "W", "W", "W", "W"],
+#   ["W", "W", "W", "W", "W", "W", "W", "W"],
+# ]
+# print(bestBridge(grid)) # -> 3
 
-# test_04:
+# # test_04:
 
-grid = [
-  ["L", "L", "L", "L", "L", "L", "L", "L"],
-  ["L", "W", "W", "W", "W", "W", "W", "L"],
-  ["L", "W", "W", "W", "W", "W", "W", "L"],
-  ["L", "W", "W", "W", "W", "W", "W", "L"],
-  ["L", "W", "W", "W", "W", "W", "W", "L"],
-  ["L", "W", "W", "W", "W", "W", "W", "L"],
-  ["L", "W", "W", "L", "W", "W", "W", "L"],
-  ["L", "W", "W", "W", "W", "W", "W", "L"],
-  ["L", "W", "W", "W", "W", "W", "W", "L"],
-  ["L", "W", "W", "W", "W", "W", "W", "L"],
-  ["L", "W", "W", "W", "W", "W", "W", "L"],
-  ["L", "L", "L", "L", "L", "L", "L", "L"],
-]
-print(bestBridge(grid)) # -> 2
+# grid = [
+#   ["L", "L", "L", "L", "L", "L", "L", "L"],
+#   ["L", "W", "W", "W", "W", "W", "W", "L"],
+#   ["L", "W", "W", "W", "W", "W", "W", "L"],
+#   ["L", "W", "W", "W", "W", "W", "W", "L"],
+#   ["L", "W", "W", "W", "W", "W", "W", "L"],
+#   ["L", "W", "W", "W", "W", "W", "W", "L"],
+#   ["L", "W", "W", "L", "W", "W", "W", "L"],
+#   ["L", "W", "W", "W", "W", "W", "W", "L"],
+#   ["L", "W", "W", "W", "W", "W", "W", "L"],
+#   ["L", "W", "W", "W", "W", "W", "W", "L"],
+#   ["L", "W", "W", "W", "W", "W", "W", "L"],
+#   ["L", "L", "L", "L", "L", "L", "L", "L"],
+# ]
+# print(bestBridge(grid)) # -> 2
 
-# test_05:
+# # test_05:
 
-grid = [
-  ["W", "L", "W", "W", "W", "W", "W", "W"],
-  ["W", "L", "W", "W", "W", "W", "W", "W"],
-  ["W", "W", "W", "W", "W", "W", "W", "W"],
-  ["W", "W", "W", "W", "W", "W", "W", "W"],
-  ["W", "W", "W", "W", "W", "W", "W", "W"],
-  ["W", "W", "W", "W", "W", "W", "L", "W"],
-  ["W", "W", "W", "W", "W", "W", "L", "L"],
-  ["W", "W", "W", "W", "W", "W", "W", "L"],
-]
-print(bestBridge(grid)) # -> 8
+# grid = [
+#   ["W", "L", "W", "W", "W", "W", "W", "W"],
+#   ["W", "L", "W", "W", "W", "W", "W", "W"],
+#   ["W", "W", "W", "W", "W", "W", "W", "W"],
+#   ["W", "W", "W", "W", "W", "W", "W", "W"],
+#   ["W", "W", "W", "W", "W", "W", "W", "W"],
+#   ["W", "W", "W", "W", "W", "W", "L", "W"],
+#   ["W", "W", "W", "W", "W", "W", "L", "L"],
+#   ["W", "W", "W", "W", "W", "W", "W", "L"],
+# ]
+# print(bestBridge(grid)) # -> 8
