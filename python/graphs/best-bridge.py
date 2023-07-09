fa68a5df96d44d1095acaa4b07308dd7dcfff472
Johnny San <johnny.san@gmail.com>
2023-07-08 21:22:34 -0700

    solved best bridge

--- python/graphs/best-bridge.py
+++ python/graphs/best-bridge.py
@@ -20,23 +20,50 @@ def bestBridge(grid):
   queue = []
 
   for pos in island:
-    print(pos, 'current pos')
+    [row, col] = pos.split(',')
+    queue.append([row, col, 0])
+
+  while len(queue) > 0:
+    [row, col, distance] = queue.pop(0)
+
+    pos = str(row) + ',' + str(col)
+
+    if grid[int(row)][int(col)] == 'L' and pos not in island:
+      return distance - 1
+
+    deltas = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+    for delta in deltas:
+      [delta_row, delta_col] = delta
+      neighbor_row = int(row) + delta_row
+      neighbor_col = int(col) + delta_col
+      neighbor_pos = str(neighbor_row) + ',' + str(neighbor_col)
+
+      if is_inbounds(grid, neighbor_row, neighbor_col) and neighbor_pos not in visited:
+        visited.add(neighbor_pos)
+        queue.append([neighbor_row, neighbor_col, distance + 1])
 
 
 def is_inbounds(grid, row, col):
   row_inbounds = True if row >= 0 and row < len(grid) else False
   col_inbounds = True if 0 <= col and col < len(grid[0]) else False
 
-  return row_inbounds and col_inbounds
+  if row_inbounds == True and col_inbounds == True:
+    return True
+  else:
+    return False
 
 
 
 def find_island(grid, row, col, visited):
   if is_inbounds(grid, row, col) == False or grid[row][col] == 'W':
-    print('not an island')
+    # print('not an island')
     return visited
 
   node = str(row) + ',' + str(col)
+
+  if node in visited:
+    return visited
 
   visited.add(node)
 
